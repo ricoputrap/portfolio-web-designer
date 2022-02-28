6f6b5a940eafb5358d29230954e68bfdb35e85df
Rico Putra Pradana <pradanaricoputra@gmail.com>
2022-02-28 12:30:08 +0700

    implement static Hero section

--- src/pages/Home/index.styles.ts
+++ src/pages/Home/index.styles.ts
@@ -1,6 +1,33 @@
 import styled from "styled-components";
+import { Heading1, Heading5 } from "../../components/core-ui/atoms/Headings";
+import { Text } from "../../components/core-ui/atoms/Text";
 
 export const Hero = styled.div`
   background-color: #28293E;
   padding: 64px 135px 139px 135px;
+
+  & .hero__wrapper {
+    display: flex;
+    margin-top: 122px;
+
+    & .hero__wrapper__text {
+      margin-right: 142px;
+      padding-top: 83px;
+      
+      ${Heading5} {
+        margin-bottom: 24px;
+        margin-top: 0;
+      }
+
+      ${Heading1} {
+        margin-top: 0;
+        margin-bottom: 24px;
+      }
+
+      ${Text} {
+        margin-top: 0;
+        margin-bottom: 34px;
+      }
+    }
+  }
 `;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import { ButtonPrimary } from '../../components/core-ui/atoms/Button';
+import { Heading1, Heading5 } from '../../components/core-ui/atoms/Headings';
+import { Text } from '../../components/core-ui/atoms/Text';
 import NavMenu from '../../components/core-ui/organisms/NavMenu';
 import useTextInput from '../../hooks/useTextInput'
 import { Hero } from './index.styles';
+import HeroImage from "../../assets/images/pic.png";
 
 const Home: React.FC = () => {
   const [value, onChange] = useTextInput();
@@ -10,6 +14,17 @@ const Home: React.FC = () => {
     <div>
       <Hero>
         <NavMenu />
+        <div className="hero__wrapper">
+          <div className="hero__wrapper__text">
+            <Heading5>GABRIEL PIRES</Heading5>
+            <Heading1 isLight>The Simple, Clean Design</Heading1>
+            <Text isLightGray>Agency provides a full service range including technical skills, design, business understanding.</Text>
+            <ButtonPrimary onClick={() => console.log("")}>See My Work</ButtonPrimary>
+          </div>
+          <div>
+            <img src={HeroImage} alt="hero-image" />
+          </div>
+        </div>
       </Hero>
      
     </div>
